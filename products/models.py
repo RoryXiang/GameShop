80618384ef6e1b4ddcd2051426b5d7839aeab592
RoryXiang <pingping1990@gmail.com>
2021-07-16 01:39:41 +0800

    first commit

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Products(models.Model):
     # identifier = models.CharField(max_length=40, unique=True)
     USERNAME_FIELD = 'identifier'
     name = models.CharField(max_length=200)
-    pic_ame = models.CharField(max_length=50)
+    # pic_ame = models.CharField(max_length=50)
     price = models.FloatField()
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -6,25 +6,27 @@ from django.contrib.auth.decorators import login_required
 from .models import Products
 from django.core import serializers
 import json
-
-
-# def list(request):
-#     products = Products.objects.filter()
-#     for p in products:
-#         print(p.name, p.price)
-#     print(products)
+from .models import Products
+from User.models import User
 
 
 def buy(request, product_id):
-    pass
+    user_name = request.session.get("user_name")
+    user = User.objects.filter(user_name=user_name).first()
+    product = Products.objects.filter(id=product_id)
+    if int(user.yue) < int(product.price):
+        return {"code": 444, "message": "购买失败"}
+    user.yue -= product.price
+    user.save()
+    return {"code": 200, "message": "购买成功", "yue": user.yue - product.price}
 
 
 def index(request):
     form = []
     if request.session.get("user_name"):
         products = Products.objects.filter()
-        result_serialize = serializers.serialize('json',products)
-        form = json.loads(result_serialize) # 对序列化之后的str类型数据进行转化为json对象
+        result_serialize = serializers.serialize('json', products)
+        form = json.loads(result_serialize)  # 对序列化之后的str类型数据进行转化为json对象
         form = [x["fields"] for x in form]
     print(form)
     return render(request, 'index.html', context={'products': form})
